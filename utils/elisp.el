4dabc06b68718306f8bd663493359e06b47766de
Daniel Neel <dneelyep@gmail.com>
2014-09-24 00:28:42 -0400

    Initial project checkin.

---
+++ utils/elisp.el
@@ -0,0 +1,5 @@
+;; This file will contain all of the code used to generate/work with the site.
+(defun make-site ()
+  "Generates a set of HTML pages, using our current set of input files."
+  (interactive)
+  (message "Testing!"))
